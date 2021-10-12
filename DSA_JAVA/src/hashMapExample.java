ce24b07b3d3da84a086c9e4cef1d5e068692f3eb
shat1959 <92391567+shat1959@users.noreply.github.com>
2021-10-12 22:27:52 +0530

    Another sample code added

--- DSA_JAVA/src/hashMapExample.java
+++ DSA_JAVA/src/hashMapExample.java
@@ -35,5 +35,26 @@ public class GFG {
             System.out.println("value for key"
                                + " \"vishal\" is:- " + a);
         }
+     
+      // No need to mention the
+        // Generic type twice
+        Map<Integer, String> hm1 = new HashMap<>();
+ 
+        // Add Elements using put method
+        hm1.put(1, "one");
+        hm1.put(2, "two");
+        hm1.put(3, "three");
+ 
+        // Initialization of a HashMap
+        // using Generics
+        HashMap<Integer, String> hm2
+            = new HashMap<Integer, String>(hm1);
+ 
+        System.out.println("Mappings of HashMap hm1 are : "
+                           + hm1);
+       
+        System.out.println("Mapping of HashMap hm2 are : "
+                           + hm2);
+ 
     }
 }
